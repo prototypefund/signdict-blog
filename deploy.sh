973bf08b321a3b997a72ed0854d873160fa66ac7
Bodo Tasche <bodo@wannawork.de>
2017-03-02 16:20:42 +0100

    changed deploy path

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bundle exec jekyll build && rsync -avz --delete -e ssh _site/ bodo02@cygnus.uberspace.de:/var/www/virtual/bodo02/signdict.org
+bundle exec jekyll build && rsync -avz --delete -e ssh _site/ bodo02@cygnus.uberspace.de:/var/www/virtual/bodo02/blog.signdict.org
