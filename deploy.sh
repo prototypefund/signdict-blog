a77a12a38bfc09cb81897ec29e8d804fbcf1e693
Bodo Tasche <bodo@wannawork.de>
2018-01-16 11:42:47 +0100

    using github pages for hosting

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,37 @@
 #!/usr/bin/env bash
-bundle exec jekyll build && rsync -avz --delete -e ssh _site/ bodo02@cygnus.uberspace.de:/var/www/virtual/bodo02/blog.signdict.org
+
+# Exit if any subcommand fails.
+set -e
+
+echo "Started deploying"
+
+# Checkout gh-pages branch.
+if [ `git branch | grep gh-pages` ]
+then
+  git branch -D gh-pages
+fi
+git checkout -b gh-pages
+
+# Build site.
+bundle exec jekyll build
+
+# Delete and move files.
+find . -maxdepth 1 ! -name '.' ! -name '..' ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+rm config.ru
+rm favicon.ico
+cp theme/favicon.ico .
+rm -rf theme
+
+git add -fA
+git commit --allow-empty -m "$(git log -1 --pretty=%B)"
+git push origin gh-pages --force
+
+# Move back to previous branch.
+git checkout -
+git submodule update
+
+echo "Deployed Successfully!"
+
+exit 0
